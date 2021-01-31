563fff5d4b2fa443c07fcee6fb7e70e59db7a09e
portolucas <lucasportosd@gmail.com>
2021-01-30 23:15:55 -0300

    Little things

--- src/services/auth/currentUser.js
+++ src/services/auth/currentUser.js
@@ -1,15 +1,5 @@
 import http from "../httpService";
 
-/**
- * 
- * @param {object} body
- * Exemple:
- * {
-	"username": "debora",
-	"password": "debora"
-  } 
- */
-
 export function getCurrentUser() {
   return http.get(`/merit_money/current_user/`);
 }
